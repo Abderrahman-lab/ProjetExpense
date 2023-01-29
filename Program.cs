b0f5d71c64bf7e4b34dc2321f625c2b4e146f970
Abderrahmane Saidi <56042977+Abderrahman-lab@users.noreply.github.com>
2023-01-29 22:22:33 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+using Expense_Tracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<Applicationdbcontex>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("BloggingDatabase")));
+
+//Register Syncfusion license
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Mgo+DSMBMAY9C3t2VVhiQlFaclxJXGFWfVJpTGpQdk5xdV9DaVZUTWY/P1ZhSXxRdk1iUH5bcHZQTmZdWU0=");
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Dashboard}/{action=Index}/{id?}");
+
+app.Run();
